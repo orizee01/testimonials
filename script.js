290d0f50fabde0f7747c9b1cb889d8a159255a2b
oreoluwa <gifttee2004@gmail.com>
2022-04-22 18:16:25 +0100

    add html, css and js

--- script.js
+++ script.js
@@ -1,26 +1,96 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+const reviews = [
+  {
+    name: "Andrew",
+    image: "img.andrew.jpeg",
+    Text: "I teach people how to sell their book online. The less code.they neec the better. zapier eliminates the need for code literture from 45BC making it over 2000 years old.",
+    writer: "Grace Garey",
+  },
+   {
+    name: "sandy",
+    image: "img.sandy.jpeg",
+    Text:"Contrary to popular belief, lorem lpsum is not simply random text. it has root in a piece of classical latin literture from 45BC making it over 2000 years old.",
+    writer: "Richard McClintock",
+     
+  },
+   {
+    name: "jessica",
+    image: "img.jessica .jpeg",
+    Text:"I teach people how to sell their book online. The less code.they neec the better. zapier eliminates the need for code literture from 45BC making it over 2000 years old.",
+    writer: "Lawrence Watins",
+   },
+   {
+    name: "David",
+    image: "img.david.jpeg",
+    Text:"Contrary to popular belief, lorem lpsum is not simply random text. it has root in a piece of classical latin literture from 45BC making it over 2000 years old.",
+    writer: "Paul jarvis",
+  },
+  {
+ name: "JOY",
+    image: "img.joy.jpeg",
+    Text:"I teach people how to sell their book online. The less code.they neec the better. zapier eliminates the need for code literture from 45BC making it over 2000 years old.",
+    writer: "Ophelie Lechat",
+  },
+  
+];
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+const img = document.getElementById("picture");
+const author = document.getElementById("author");
+const info = document.getElementById("info");
+const writer = document.getElementById("writer");
+
+
+
+const prevbtn = document.getElementById("prev-btn");
+const nextbtn = document.getElementById("next-btn");
+
+//set  starting  item
+let slide = 0;
+  
+//load initial item
+window.addEventListener("DOMContentLoaded" , function () {
+  showPerson(slide)  
+});
+
+//each person function
+function showPerson(e) {
+   const item = reviews[e];
+  img.src = item.image;
+ author.innerHTML= item.name;
+ info.textContent = item.Text;
+ writer.textContent = item.writer
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+nextbtn.addEventListener("click", function () { 
+  slide++;
+if (slide > reviews.length - 1) {
+    slide = 0;
+  }
+  showPerson(slide);
+});
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("carousel-theme");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+prevbtn.addEventListener("click", function () {
+  slide--;
+  if (slide < 0) {
+    slide = reviews.length - 1;
+  }
+  showPerson(slide);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
